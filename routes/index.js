fa222d5b247b5d64d78ecfa54379c21bae7e847d
Khalifa-Alqiadi <khalifa.alqiadi@gmail.com>
2022-02-18 21:12:49 +0300

    add index.ejs file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
+const axios = require('axios')
 var router = express.Router();
 
 /* GET home page. */
 router.get('/',  function(req, res, next){
-    res.render('index', {title: 'home' });
+  axios.get("http://dummyjson.com/products")
+  .then((response) =>  {
+    res.render('index', {title: 'home', products: response.data.products });
+  }).catch((err) =>{
+    console.log("err")
+  })
+  
 });
 
 module.exports = router;
