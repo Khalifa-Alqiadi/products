c732e9eb7399bc48d308efd800c0866b1d89ddca
Khalifa-Alqiadi <khalifa.alqiadi@gmail.com>
2022-02-19 15:27:34 +0300

    edit all file

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/details', detailsRouter);
 app.use('/category', categoriesRouter);
+app.use('/categories', categories);
 app.use('/search', searchRouter);
 
 // catch 404 and forward to error handler
